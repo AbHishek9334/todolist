d17580a3deea77a6be3942a7b70c7c6105adc592
AbHishek9334 <narayana333434@gmail.com>
2024-07-30 11:41:53 +0530

    todolist with context api

---
+++ src/component/Todo/Todo.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react"
+
+function Todo({id,todoData,isFinished,changeFinished,onDelete,onEdit}){
+    const [finished,setFinished]=useState(isFinished)
+    const [isEditing,setIsEditing]=useState(false)
+    const [editText,setEditText]=useState("")
+    return(
+        <div>
+            <input type="checkbox" checked={finished} onChange={(e)=>{setFinished(e.target.checked)
+                                                                    changeFinished(e.target.checked)
+            }}/>
+
+            {(isEditing)?<input type="text" value={editText} onChange={(e)=>setEditText(e.target.value)}/>:todoData}
+            <button onClick={()=>{
+                setIsEditing(!isEditing)
+                onEdit(editText)
+            }}>{(!isEditing)?"Edit":"Save"}</button>
+            <button onClick={onDelete}>Delete</button>
+        
+        </div>
+    )
+
+}
+export default Todo
