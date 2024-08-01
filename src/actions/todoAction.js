5d883b10b2fad4ae215951a8a0e2c60ef8fc00a3
AbHishek9334 <126397781+AbHishek9334@users.noreply.github.com>
2024-08-01 12:34:23 +0530

    Delete src directory

--- src/actions/todoAction.js
+++
@@ -1,6 +0,0 @@
-import { FINISH_TODO,ADD_TODO,DELETE_TODO,Edit_TODO } from "../Constants/actions"
-
-export const todoFinished=(todo,isFinished)=>({type:FINISH_TODO,payload:{todo,isFinished}})
-export const addTodo=(todoText)=>({type:ADD_TODO,payload:{todoText:todoText}})
-export const deleteTodo=(todo)=>({type:DELETE_TODO,payload:{todo}})
-export const editTodo=(todo,todoText)=>({type:Edit_TODO,payload:{todo,todoText}})
